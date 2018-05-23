ccd51229423dc70c64562c709d0a2c165a854957
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2018-05-23 16:14:18 +0200

    Fix setup

--- setup.py
+++ setup.py
@@ -6,23 +6,24 @@ from setuptools import setup
 # README file and 2) it's easier to type in the README file than to put a raw
 # string in below ...
 def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+    with open(os.path.join(os.path.dirname(__file__), fname)) as f:
+        content =  f.read()
+    return content
 
 setup(
     name = "fact_conditions",
     version = "0.1.0",
     author = "Michael Bulinski",
     author_email = "michael.bulinski@udo.edu",
-    description = ("Functions to load conditions for the fact quary. Also includes a veriety of given files."),
-    license = "GPL3",
-    keywords = "fact database query condition",
+    description = "Functions to load conditions for the datecheck query. Also includes a veriety of preconfigured files.",
+    license = "GPLv3",
     url = "https://github.com/fact-project/fact_condition",
     packages=['fact_conditions'],
     long_description=read('README.md'),
     install_requires=[
-        pyyaml,
-        click,
-        peewee
+        'pyyaml',
+        'click',
+        'peewee',
     ],
     package_data={'fact_conditions': ['conditions/*.yaml']},
     entry_points={
